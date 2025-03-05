efe4bf91f33059340a1298262e9ab71afcd918ec
jkchoi490 <jkchoi490@daum.net>
2025-03-05 10:23:58 +0900

    refactor: "white", "black" 문자에 대한 중복 제거를 위한 로컬 변수 생성

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -9,12 +9,15 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() { // create() 테스트 메소드 작성
-        Pawn pawn = new Pawn("white");
-        assertThat(pawn.getColor()).isEqualTo("white");
+        String white = "white"; //local variable을 추가
+        String black = "black";
+
+        Pawn pawn = new Pawn(white);
+        assertThat(pawn.getColor()).isEqualTo(white);
 
         //2. Pawn 클래스 구현
-        Pawn blackPawn = new Pawn("black");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+        Pawn blackPawn = new Pawn(black);
+        assertThat(blackPawn.getColor()).isEqualTo(black);
 
     }
 }
