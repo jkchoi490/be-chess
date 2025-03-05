30508e98875ff34a34106c05e3729da77c494b21
jkchoi490 <jkchoi490@daum.net>
2025-03-05 10:18:37 +0900

    feat: 1. Pawn 테스트 - PawnTest 클래스 생성, create() 테스트 메소드 작성, 2. Pawn 클래스 구현 - 검은색 폰 생성

---
+++ src/main/java/org/example/Pawn.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Pawn {
+    String color;
+
+    public Pawn(String color) {
+        this.color=color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/test/java/PawnTest.java
@@ -0,0 +1,20 @@
+import org.example.Pawn;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색 폰이 생성되어야 한다")
+    public void create() { // create() 테스트 메소드 작성
+        Pawn pawn = new Pawn("white");
+        assertThat(pawn.getColor()).isEqualTo("white");
+
+        //2. Pawn 클래스 구현
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
+
+    }
+}
